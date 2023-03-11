fa086727e25a7d45919f638cde38852819eb7492
Lara Antunes <eularaantunes@gmail.com>
2023-03-10 21:42:10 -0300

    Including topic validation

--- otto.go
+++ otto.go
@@ -1,5 +1,10 @@
 package otto
 
+import (
+	"fmt"
+	"runtime"
+)
+
 // Subscriber Defines a interface for Pub/Sub subscribers on a topic
 type Subscriber interface {
 	Handle(payload interface{})
@@ -16,19 +21,37 @@ func RegisterTopics(topicsToRegister ...string) {
 }
 
 // RegisterSubscribers Allows to register subscribers on topics
-func RegisterSubscribers(topic string, subscribersToRegister ...Subscriber) {
+func RegisterSubscribers(topic string, subscribersToRegister ...Subscriber) error {
+	if !topicExists(topic) {
+		return fmt.Errorf("Topic %s is not registered", topic)
+	}
 	for _, subscriber := range subscribersToRegister {
 		subscribers[topic] = append(subscribers[topic], subscriber)
 	}
+	return nil
 }
 
 // Publish Allows to publish on a specific topic, throwing a broadcast for
 // all subscribers of the topic
-func Publish(topic string, payload interface{}) {
+func Publish(topic string, payload interface{}) error {
+	if !topicExists(topic) {
+		return fmt.Errorf("Topic %s is not registered", topic)
+	}
 	if len(subscribers[topic]) == 0 {
-		return
+		return nil
 	}
 	for _, subscriber := range subscribers[topic] {
+		runtime.Gosched()
 		go subscriber.Handle(payload)
 	}
+	return nil
+}
+
+func topicExists(topic string) bool {
+	for _, t := range topics {
+		if t == topic {
+			return true
+		}
+	}
+	return false
 }
